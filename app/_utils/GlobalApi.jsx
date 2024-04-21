911e17449e41027b804d514fd83f2dec5c0765ac
saurav-ghimire <sauravghimire0123@gmail.com>
2024-04-21 19:51:10 -0400

    URL updated for strapi

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY; 
 const axiosClient = axios.create({
-  baseURL:'http://localhost:1337/api/',
+  baseURL:'https://doctor-appointment-booking-admin-strapi.onrender.com/api',
   headers:{
     'Authorization':`Bearer ${API_KEY}`
   }
